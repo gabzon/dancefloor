8779cd08b5712fea89b22d7b4afad8455bd95f40
gabzon <gab.zambrano@gmail.com>
2016-04-28 13:09:19 +0200

    gravity editor

--- piklist/parts/settings/settings.php
+++ piklist/parts/settings/settings.php
@@ -3,7 +3,7 @@ Title: Dancefloor Settings
 Setting: dancefloor_settings
 */
 piklist('field', array(
-    'type'          => 'text',
+    'type'          => 'editor',
     'field'         => 'registration_form',
     'label'         => __('Registration form','sage'),
     'description'   => __('Copy and paste gravity form shortcode','sage'),
