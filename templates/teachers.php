d31d8719a92ffd70dd09223e884083134d31862d
gabzon <gab.zambrano@gmail.com>
2016-05-02 01:11:23 +0200

    teachers title

--- templates/teachers.php
+++ templates/teachers.php
@@ -88,7 +88,7 @@ function display_person($indice, $id, $email, $photo, $first_name, $last_name, $
 
 <?php $profs = get_users( 'role=teacher' ); ?>
 <?php $indice = 100; ?>
-<h2><?php _e('Teachers','sage'); ?></h2>
+<h2><?php _e('Company Dansers','sage'); ?></h2>
 <div class="ui special four stackable cards">
     <?php foreach ( $profs as $user ) :?>
         <!-- Card -->
@@ -102,7 +102,7 @@ function display_person($indice, $id, $email, $photo, $first_name, $last_name, $
 <br>
 <?php $assistants = get_users( 'role=assistant' ); ?>
 <?php $indice = 0; ?>
-<h2><?php _e('The team','sage'); ?></h2>
+<h2><?php _e('Teachers','sage'); ?></h2>
 <div class="ui special four stackable cards">
     <?php foreach ( $assistants as $user ) :?>
         <!-- Card -->
